434b1246ab0fb9d0e7e9e06f23ca3e4bd0302450
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-13 22:24:31 +0600

    add dynamic route

--- client/src/components/Rooms/Categories/CategoryBox.jsx
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import qs from 'query-string'
 
-const CategoryBox = ({ label, icon: Icon }) => {
+const CategoryBox = ({ label, icon: Icon,selected}) => {
+  console.log(selected)
   const [params, setParams] =useSearchParams()
   const navigate=useNavigate()
   const handleClick=()=>{
     let currentQuery={}
     if(params){
       currentQuery=qs.parse(params.toString)
-
+    }
       const updateQuery={...currentQuery,category:label}
 
       const url=qs.stringifyUrl({
@@ -17,11 +18,11 @@ const CategoryBox = ({ label, icon: Icon }) => {
         query:updateQuery
       })
       navigate(url)
-    }
+    
   }
   params.get("category")
   return (
-    <div onClick={handleClick} className='flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer '>
+    <div onClick={handleClick} className={`flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? "border-b-neutral-800 text-neutral-800" :""} `}>
       {<Icon size={26}></Icon>}
       <div className='text-sm font-medium'>
         {label}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 
+
 import Categories from "../../components/Rooms/Categories/Categories"
 import Rooms from "../../components/Rooms/Rooms"
 
 const Home = () => {
+ 
   return (
     <div>
       <Categories></Categories>
       <Rooms></Rooms>
-      
+
     </div>
   )
 }
