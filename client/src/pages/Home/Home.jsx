fa7cb080c9c345b498e984b1ebb4ad3cfda5b02d
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-14 15:24:12 +0600

    create dynamic route

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 
+import { Helmet } from "react-helmet-async"
 import Categories from "../../components/Rooms/Categories/Categories"
 import Rooms from "../../components/Rooms/Rooms"
 
@@ -7,6 +8,9 @@ const Home = () => {
  
   return (
     <div>
+      <Helmet>
+        <title> Hotel | Hotel Management</title>
+      </Helmet>
       <Categories></Categories>
       <Rooms></Rooms>
 
