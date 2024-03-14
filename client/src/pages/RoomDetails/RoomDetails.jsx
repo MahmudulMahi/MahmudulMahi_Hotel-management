90e58f1aa05012ebd317365da7c11246967740c8
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-14 23:14:58 +0600

    add react deat picker

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -3,6 +3,10 @@ import Container from '../../components/Shared/Container';
 import { useParams } from 'react-router-dom';
 import Loader from '../../components/Shared/Loader';
 import { Helmet } from 'react-helmet-async';
+import Header from '../../components/RoomDetails/Header';
+import RoomInfo from './RoomInfo';
+import Calender from './Calender';
+import RoomReservation from './RoomReservation';
 
 const RoomDetails = () => {
 
@@ -32,7 +36,18 @@ const RoomDetails = () => {
     <Helmet>
     <title>{room?.title}</title>
     </Helmet>
-        {room.title}
+    <div className='max-w-screen-lg mx-auto'>
+      <div className='flex flex-col'>
+      <Header room={room}></Header>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-10'>
+        <RoomInfo room={room}></RoomInfo>
+        <div className='md:col-span-3 order-first md:order-last mb-10'>
+        <RoomReservation room={room}></RoomReservation>
+        </div>
+      </div>
+    </div>
+     
       </Container>
     </div>
   );
