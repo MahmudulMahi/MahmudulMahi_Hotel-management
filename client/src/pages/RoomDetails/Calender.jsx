2de237404ff8216949d249b87eb747f5b05c7ee7
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-22 23:32:15 +0600

    create signup function

---
+++ client/src/pages/RoomDetails/Calender.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { DateRange } from 'react-date-range'
+
+// eslint-disable-next-line react/prop-types
+const Calender = () => {
+  return (
+    <DateRange
+      rangeColors={['#F43F5E']}
+      
+      
+  
+     
+    />
+  )
+}
+
+export default Calender
