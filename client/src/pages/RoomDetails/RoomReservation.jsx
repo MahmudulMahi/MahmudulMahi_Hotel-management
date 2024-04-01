5b775e5dd95a2fae202b1598b6535f2434768e84
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-04-02 02:26:01 +0600

    create modal for room reservation

--- client/src/pages/RoomDetails/RoomReservation.jsx
+++ client/src/pages/RoomDetails/RoomReservation.jsx
@@ -4,58 +4,89 @@ import Calender from './Calender';
 import Button from '../../components/Button/Button';
 import { useState } from 'react';
 import { formatDistance } from 'date-fns';
+import BookingModal from '../../components/Model/BookingModal';
+import useAuth from '../../hooks/useAuth';
 
-const RoomReservation = ({room}) => {
 
-  const [value,setValue]=useState({
-    startDate:new Date(room?.from),
-    endDate:new Date(room?.to),
-    key:'selection',
+const RoomReservation = ({ room }) => {
+
+  let [isOpen, setIsOpen] = useState(false)
+  const {user} =useAuth()
+  const closeModal = () =>{
+    setIsOpen(false)
+  }
+
+  const [value, setValue] = useState({
+    startDate: new Date(room?.from),
+    endDate: new Date(room?.to),
+    key: 'selection',
   })
+
   const totalDays = parseInt(
-    formatDistance(new Date(room?.to) , new Date(room?.from)).split(' ')[0]
+    formatDistance(new Date(room?.to), new Date(room?.from)).split(' ')[0]
   )
-  const totalPrice= totalDays * room?.price
+  // Total Price Calculation
+  const totalPrice = totalDays * room?.price
   console.log(totalPrice)
 
-    // Total Price Calculation
-    
-    const handleDateChange = ranges => {
-      console.log(ranges)
-      setValue({
-        startDate: new Date(room?.from),
-        endDate: new Date(room?.to),
-        key: 'selection',
-      })
-    }
+
+  
+
+  const handleDateChange = ranges => {
+    console.log(ranges)
+    setValue({
+      startDate: new Date(room?.from),
+      endDate: new Date(room?.to),
+      key: 'selection',
+    })
+  }
+
+  const [bookingInfo, setBookingInfo] = useState({
+    guest: {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    },
+    host: room?.host?.email,
+    location: room?.location,
+    price: totalPrice,
+    to: value.endDate,
+    from: value.startDate,
+    title: room?.title,
+    roomId: room?._id,
+    image: room?.image,
+  })
+
+  console.log(value)
 
   return (
     <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white  '>
       <div className='flex items-center gap-1 p-4'>
-      <div className='text-2xl font-semibold'>
-        ${room.price}
-      </div>
-      <div className='font-light text-neutral-600'>
-        night
-      </div>
+        <div className='text-2xl font-semibold'>
+          ${room.price}
+        </div>
+        <div className='font-light text-neutral-600'>
+          night
+        </div>
       </div>
       <hr />
       <div className='flex justify-center'>
-      <Calender handleDateChange={handleDateChange} value={value} ></Calender>
+        <Calender handleDateChange={handleDateChange} value={value} ></Calender>
       </div>
       <hr />
       <div className='p-4'>
-        <Button label={"Reserve"}></Button>
+        <Button onClick={() => { setIsOpen(true) }} label={"Reserve"}></Button>
       </div>
       <hr />
       <div className="p-4 flex item-center justify-between font-semibold text-lg">
-      <div>
-      Total
+        <div>
+          Total
+        </div>
+        <div>
+          $ {totalPrice}
+        </div>
       </div>
-      <div>
-      $ {totalPrice}
-      </div>
-      </div>
+      <BookingModal closeModal={closeModal} isOpen={isOpen} bookingInfo={bookingInfo}></BookingModal>
     </div>
   );
 };
