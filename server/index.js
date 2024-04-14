42a2ff9f428fab7e8a84fa4e2f76d2491e022534
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-04-14 16:54:17 +0600

    save booking information

--- server/index.js
+++ server/index.js
@@ -154,6 +154,14 @@ async function run() {
       res.send({clientSecret: client_secret})
     })
 
+    // save booking
+
+    app.post('/bookings',async(req,res)=>{
+      const booking=req.body
+      const result=await bookingsCollection.insertOne(booking)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
