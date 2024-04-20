41df5c61e50e45b03d3a14e61d053587c7b91d51
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-04-20 12:26:54 +0600

    create guast and host api

--- server/index.js
+++ server/index.js
@@ -177,6 +177,23 @@ async function run() {
       res.send(result)
     })
 
+    // get all booking for guest
+    app.get('/bookings',verifyToken,async (req,res) =>{
+      const email=req.query.email
+      if(!email) return res.send([])
+      const query={'guest.email':email}
+      const result =await bookingsCollection.find(query).toArray()
+      res.send(result)
+    })
+    // get all booking for host
+    app.get('/bookings/host',verifyToken,async (req,res) =>{
+      const email=req.query.email
+      if(!email) return res.send([])
+      const query={host:email}
+      const result =await bookingsCollection.find(query).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
