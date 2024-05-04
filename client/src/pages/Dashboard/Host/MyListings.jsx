ffd7c2aaa1fd1c74ed5476d1856d62aef4c65e20
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-05-04 23:52:13 +0600

    add listing data

--- client/src/pages/Dashboard/Host/MyListings.jsx
+++ client/src/pages/Dashboard/Host/MyListings.jsx
@@ -79,6 +79,60 @@ useEffect(()=>{
                   }
                   </tbody>
               </table>
+              <table className='min-w-full leading-normal'>
+                <thead>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Location
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      From
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      To
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Delete
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Update
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Room row data */}
+                  {
+                    rooms.map(room => <RoomDataRow key={room._id} room={room}></RoomDataRow>)
+                  }
+                  </tbody>
+              </table>
             </div>
           </div>
         </div>
