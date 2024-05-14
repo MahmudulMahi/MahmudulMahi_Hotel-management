f3458465d737636caf0c05d663ae5eb5149a2dce
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-05-14 23:23:24 +0600

    create manage user route

--- client/src/components/Dashboard/TableRows/GuestMenu.jsx
+++ client/src/components/Dashboard/TableRows/GuestMenu.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItem from '../Sidebar/MenuItem';
 import { BsFillHouseAddFill } from 'react-icons/bs';
 import { FaListOl } from 'react-icons/fa';
 import useRole from '../../../hooks/useRole'
 import { GrUserAdmin } from 'react-icons/gr'
+import useAuth from '../../../hooks/useAuth';
+import HostModal from '../../Model/HostRequestModal';
 const GuestMenu = () => {
   const [role] = useRole()
+  const {user}=useAuth()
+  const [isOpen,setIsOpen]=useState(true)
+
+  const modelHandler=async () =>{
+
+  }
   return (
     <>
       <MenuItem
@@ -20,7 +28,7 @@ const GuestMenu = () => {
           <span className='mx-4 font-medium'>Become A Host</span>
         </div>
       )}
-
+    <HostModal></HostModal>
     </>
   );
 };
